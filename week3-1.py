9c3e4269c702250227d69880f4af4a9720a316bd
agaz2006 <agaz2006@yahoo.com>
2020-05-11 23:58:27 +0300

    week3-1

---
+++ week3-1.py
@@ -0,0 +1,10 @@
+print ('Gussing Number Game \n')
+x=6
+while True :
+    i=int(input ('Enter the Number 1 To 10 : '))
+    if x==i :
+        print ('Graet You did it')
+        break
+    else :
+        print ('Try it again ')
+        continue
